29fd9186ee696fc4dd238f2b5a48b91fa6bb6e50
Faruk <52610005+farukyo@users.noreply.github.com>
2025-10-31 20:59:40 +0300

    Add new pages and navigation, update routes and styles
    Replaced the 'information' page with a new 'about' page, added new routes and templates for 'series', 'celebrities', and 'recommend' pages, and updated navigation across templates for consistency. Introduced new CSS files for 'about' and 'movies' pages, and removed obsolete 'information' files. Improved overall site structure and navigation.

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app= Flask(__name__)
 def home_page():
     return render_template("home.html")
 
-@app.route("/information")
-def information_page():
-    return render_template("information.html")
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 @app.route("/quiz")
 def quiz_page():
@@ -21,6 +21,14 @@ def recommend_page():
 @app.route("/movies")
 def movies_page():
     return render_template("movies.html")
+
+@app.route("/series")
+def series_page():
+    return render_template("series.html")
+
+@app.route("/celebrities")
+def celebrities_page():
+    return render_template("celebrities.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
