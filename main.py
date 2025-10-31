87222d56e59ae968167454883558208dac2b75b3
eylulebrar <eylulebrargulbeyaz@gmail.com>
2025-10-31 20:01:18 +0300

    Movies page first draft for html

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/information")
+def information_page():
+    return render_template("information.html")
+
+@app.route("/quiz")
+def quiz_page():
+    return render_template("quiz.html")
+
+@app.route("/recommend")
+def recommend_page():
+    return render_template("recommend.html")
+
+@app.route("/movies")
+def movies_page():
+    return render_template("movies.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
